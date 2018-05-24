60ed44d2fa7a76c8fa7373c76f2cf5455e57200b
vicky1408 <vigneshv01@gmail.com>
2018-05-23 23:46:17 -0400

    Added Bootstrap, jQuery, PopperJS. Created index page with card like UI.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'விழித்திருப்போம் / Vizhithirupom' });
 });
 
 module.exports = router;
