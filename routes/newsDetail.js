de763407b9c0b713548d114ff3bbeb5ca3aece49
vicky1408 <vigneshv01@gmail.com>
2018-05-29 23:17:49 -0400

    Added basic MongoDB link to the application.

--- routes/newsDetail.js
+++ routes/newsDetail.js
@@ -6,5 +6,21 @@ router.get('/:newsId', function(req, res) {
   res.render('newsDetail',{title: req.params.newsId});
 });
 
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/dbVpm', function (err, client) {
+  if (err) throw err
+
+  var db = client.db('dbVpm')
+  
+  db.collection('teams').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
+
 
 module.exports = router;
+
+
