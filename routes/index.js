0656ea6e216c785a5957d85aae97d4a21a8155b2
vicky1408 <vigneshv01@gmail.com>
2018-05-30 23:30:22 -0400

    MongoDB demo collection created. Populated the same in index and news detail page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+//Connect to DB
+var MongoClient = require('mongodb').MongoClient
+var newsCollectionResult;
+
+MongoClient.connect('mongodb://localhost:27017/dbVpm', function (err, client) {
+  if (err) throw err
+
+  var db = client.db('dbVpm')
+  
+  db.collection('newsCollection').find().toArray(function (err, result) {
+    if (err) throw err
+    newsCollectionResult = result;
+    //console.log(newsCollectionResult)
+  })
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'விழித்திருப்போம் / Vizhithirupom' });
+  res.render('index', { title: 'விழித்திருப்போம் / Vizhithirupom', newsCollectionList : newsCollectionResult });
 });
+
 
 module.exports = router;
--- routes/newsDetail.js
+++ routes/newsDetail.js
@@ -1,25 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient
+var newsCollectionResult;
+
 /* GET News Detail. */
 router.get('/:newsId', function(req, res) {
-  res.render('newsDetail',{title: req.params.newsId});
-});
 
-var MongoClient = require('mongodb').MongoClient
-
-MongoClient.connect('mongodb://localhost:27017/dbVpm', function (err, client) {
-  if (err) throw err
-
-  var db = client.db('dbVpm')
-  
-  db.collection('teams').find().toArray(function (err, result) {
+  var postId = req.params.newsId;
+  //Connect to DB
+  MongoClient.connect('mongodb://localhost:27017/dbVpm', function (err, client) {
     if (err) throw err
 
-    console.log(result)
+    var db = client.db('dbVpm')
+    
+    db.collection('newsCollection').find({
+      $and:[{"postId":postId}]
+    }).toArray(function (err, result) {
+      if (err) throw err
+      newsCollectionResult = result;
+    })
   })
-})
 
+  console.log(newsCollectionResult)
+  res.render('newsDetail',{title: req.params.newsId, newsDocument: newsCollectionResult});
+});
 
 module.exports = router;
 
