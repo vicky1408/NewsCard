113764694750b54c26907ec0ed35dc625a481b2a
vicky1408 <vigneshv01@gmail.com>
2018-06-04 23:25:40 -0400

    Created form for News submission:

--- routes/createNews.js
+++ routes/createNews.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
+
+
+router.get('/', function(req, res) {
+    res.render('createNews',{site: 'விழித்திருப்போம் / Vizhithirupom', title: 'Create News'});
+});
+
+module.exports = router;
